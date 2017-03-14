ad0adc564bebbdb6d9b89f2f2f4dee2fc9d8366b
kxxoling <kxxoling@gmail.com>
2017-03-15 01:19:34 +0800

    Rewrite in vue style.

--- src/components/MokouPlayer.vue
+++ src/components/MokouPlayer.vue
@@ -1,15 +1,16 @@
 <template>
   <section class="video-player">
-    <input class="source" type="file" accept=".mp4,.mkv,.webm,.webp">
-    <button class="snapshot">snapshot</button>
-    <button class="forward">forward</button>
-    <button class="backward">backward</button>
+    <h1>{{ this.file && this.file.name }}</h1>
+    <input class="source" type="file" accept=".mp4,.mkv,.webm,.webp" v-on:change="onVideoChanged" multiple="false">
+    <button class="snapshot" @click="takeSnapshot">snapshot</button>
+    <button class="forward" @click="forward">forward</button>
+    <button class="backward" @click="backward">backward</button>
     <video class="video" controls autoplay download>
+      <a :href="fileUrl" download>download</a>
     </video>
   </section>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   video, input {
   display: block;
@@ -29,77 +30,57 @@
 <script type="text/javascript">
 const URL = window.URL || window.webkitURL;
 
-function downloadCanvas(canv) {
-  const image = canv.toDataURL('image/png');
-  const aLink = document.createElement('a');
-  const evt = document.createEvent('HTMLEvents');
-  evt.initEvent('click');
-
-  aLink.download = 'snapshot.png';
-  aLink.href = image;
-
-  aLink.dispatchEvent(evt);
-  aLink.click();
-}
-function takeSnapshot() {
-  const video = this.parentNode.querySelector('video');
+function takeSnapshot(video, filename = 'snapshot') {
   const canvas = document.createElement('canvas');
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   const ctx = canvas.getContext('2d');
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  const image = canvas.toDataURL('image/png');
+  const aLink = document.createElement('a');
+  const evt = document.createEvent('HTMLEvents');
+  evt.initEvent('click');
 
-  downloadCanvas(canvas);
-}
+  aLink.download = `${filename}.png`;
+  aLink.href = image;
 
-function forward(seconds = 5) {
-  const video = this.parentNode.querySelector('video');
-  const currentTime = video.currentTime;
-  const end = video.seekable.end(0);
-  const next = currentTime + seconds;
-  video.currentTime = (end > next) ? next : end;
-}
-
-function backward(seconds = 5) {
-  const video = this.parentNode.querySelector('video');
-  const currentTime = video.currentTime;
-  const end = video.seekable.end(0);
-  const next = currentTime - seconds;
-  video.currentTime = (end > next) ? next : end;
-}
-
-function localFileVideoPlayer(videoNode) {
-  const parentNode = videoNode.parentNode;
-  function playSelectedFile() {
-    const file = this.files[0];
-    // const type = file.type;
-
-    const fileURL = URL.createObjectURL(file);
-    videoNode.src = fileURL;  // eslint no-param-reassign: 0
-
-    const downloadNode = document.createElement('a');
-    downloadNode.innerText = 'download';
-    downloadNode.setAttribute('href', fileURL);
-    downloadNode.setAttribute('download', 'download');
-    videoNode.innerHTML = '';
-    videoNode.appendChild(downloadNode);
-  }
-  const inputNode = parentNode.querySelector('input');
-  inputNode.addEventListener('change', playSelectedFile, false);
-  const snapshotBtn = parentNode.querySelector('.snapshot');
-  snapshotBtn.addEventListener('click', takeSnapshot, false);
-  const forwardButton = parentNode.querySelector('.forward');
-  const backwardButton = parentNode.querySelector('.backward');
-  forwardButton.addEventListener('click', forward, false);
-  backwardButton.addEventListener('click', backward, false);
+  aLink.dispatchEvent(evt);
+  aLink.click();
 }
 
 export default {
   data() {
-    return {};
+    return {
+      video: null,
+      file: null,
+      fileUrl: null,
+    };
   },
-  mounted() {
-    localFileVideoPlayer(this.$el.querySelector('video'));
+  methods: {
+    onVideoChanged() {
+      this.video = this.$el.querySelector('video');
+      const videoNode = this.video;
+      this.file = this.$el.querySelector('input').files[0];
+      this.fileURL = URL.createObjectURL(this.file);
+      videoNode.src = this.fileURL;  // eslint no-param-reassign: 0
+    },
+    forward(ev, seconds = 5) {
+      const video = this.video;
+      const currentTime = video.currentTime;
+      const end = video.seekable.end(0);
+      const next = currentTime + seconds;
+      video.currentTime = (end > next) ? next : end;
+    },
+    backward(ev, seconds = 5) {
+      const video = this.video;
+      const currentTime = video.currentTime;
+      const end = video.seekable.end(0);
+      const next = currentTime - seconds;
+      video.currentTime = (end > next) ? next : end;
+    },
+    takeSnapshot() {
+      takeSnapshot(this.video);
+    },
   },
 };
 </script>
